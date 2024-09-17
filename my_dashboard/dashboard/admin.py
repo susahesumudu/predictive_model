78bb19839f0db05e9cd31daf999c39a52d09cdff
susahesumudu <sumudu.susahe@gmail.com>
2024-09-18 00:01:01 +0530

    New dashboard system modified

--- my_dashboard/dashboard/admin.py
+++ my_dashboard/dashboard/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from .models import Metric
-from .models import Student, AttendanceRecord,Task,Schedule,Batch
+from .models import Student, AttendanceRecord,Task,Schedule,Batch,Module
 
 
 admin.site.register(Metric)
 
 
 admin.site.register(Batch)
-
+admin.site.register(Module)
 admin.site.register(Task)
 admin.site.register(Schedule)
 admin.site.register(Student)
